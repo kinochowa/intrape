68bc050382c6f7729f8961930c9db721973bd4af
Bastien DUPIC <bastien1.dupic@epitech.eu>
2018-10-02 20:13:08 +0200

    app.js: add /houses/* routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var studentsRouter = require('./routes/students');
+var housesRouter = require('./routes/houses');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/students', studentsRouter);
+app.use('/houses', housesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

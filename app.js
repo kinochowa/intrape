93980a79515a8dc3578944c42c24087676e65c5e
Bastien DUPIC <bastien1.dupic@epitech.eu>
2018-10-17 16:13:47 +0200

    debug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
 var passport = require('passport');
 var AzureAdOAuth2Strategy = require('passport-azure-ad-oauth2');
 
@@ -16,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
